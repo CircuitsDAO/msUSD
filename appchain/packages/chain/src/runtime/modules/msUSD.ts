c2584f7496523f1c8d6a062e573e728cae736782
br0wnD3v <bothereum@protonmail.com>
2024-09-08 03:51:28 +0530

    Updates to the mint, burn functions and added important getter and setters.

--- appchain/packages/chain/src/runtime/modules/msUSD.ts
+++ appchain/packages/chain/src/runtime/modules/msUSD.ts
@@ -1,3 +1,11 @@
+/// ONS
+
+/// Author : br0wnD3v
+/// Description : Business Logic for an overcollateralized stablecoin's POC written as an Appchain.
+/// Advantages : Fast - Cheap - MEV Protected - The whole state is verifiable.
+/// Mock : The price of the underlying collateral is simply set as a variable which is updated by the
+///        CircuitsDAO public key.
+
 import { State, StateMap, assert } from "@proto-kit/protocol";
 import {
   RuntimeModule,
@@ -5,8 +13,7 @@ import {
   runtimeMethod,
   state,
 } from "@proto-kit/module";
-import { UInt224, UInt32 } from "@proto-kit/library";
-
+import { UInt224 } from "@proto-kit/library";
 import { PublicKey, Field, Bool } from "o1js";
 
 interface StableConfig {
@@ -15,9 +22,12 @@ interface StableConfig {
   collateralBalances: StateMap<PublicKey, UInt224>;
   collateralSupply: UInt224;
   collateralPrice: UInt224;
-  collateralRatio: UInt32;
+  collateralRatio: UInt224;
   systemLocked: Bool;
   CircuitsDAO: PublicKey;
+  fee: UInt224;
+  feeCollected: UInt224;
+  decimals: UInt224;
 }
 
 @runtimeModule()
@@ -26,27 +36,60 @@ export class msUSD extends RuntimeModule<StableConfig> {
   @state() public stableSupply = State.from(UInt224);
   @state() public collateralBalances = StateMap.from(PublicKey, UInt224);
   @state() public collateralSupply = State.from(UInt224);
+
+  /// UPDATED REGULARLY BY THE DAO'S MULTISIG
   @state() public collateralPrice = State.from(UInt224);
+
   @state() public collateralRatio = State.from(UInt224);
+
   @state() public CircuitsDAO = State.from(PublicKey);
+
+  /// FOR FAULT TOLERENCE AND EMERGENCY DEPEGS
   @state() public systemLocked = State.from(Bool);
+
   @state() public fee = State.from(UInt224);
+  @state() public feeCollected = State.from(UInt224);
+
+  @state() public decimals = State.from(UInt224);
 
   /// SETTER METHODS ==============================================
+  /// @param toSetRatio - Default is 15-
 
   @runtimeMethod() public async init(
-    toSetRatio: UInt32,
-    toSetMultiSig: PublicKey
+    _circuitsDao: PublicKey,
+    _ratio: UInt224, /// 150% == 15e19
+    _fee: UInt224, /// 0.05% == 5e16
+    _decimals: UInt224 /// Default 18 = 1000000000000000000
   ): Promise<void> {
+    const zero = UInt224.from(0);
+
     assert((await this.collateralRatio.get()).isSome.not());
     assert((await this.CircuitsDAO.get()).isSome.not());
+    assert((await this.decimals.get()).isSome.not());
+    assert((await this.fee.get()).isSome.not());
 
-    toSetRatio.assertGreaterThan(UInt32.from(0));
-    assert(toSetMultiSig.isEmpty().not());
+    assert(_circuitsDao.isEmpty().not());
+    assert(_ratio.greaterThan(zero));
+    assert(_fee.greaterThan(zero));
+    assert(_decimals.greaterThan(zero));
 
-    this.collateralRatio.set(toSetRatio);
-    this.CircuitsDAO.set(toSetMultiSig);
-    this.fee.set(UInt224.from(5000000));
+    this.stableSupply.set(zero);
+    this.collateralSupply.set(zero);
+    this.CircuitsDAO.set(_circuitsDao);
+
+    this.systemLocked.set(Bool(false));
+    this.collateralRatio.set(_ratio);
+    this.fee.set(_fee);
+    this.decimals.set(_decimals);
+  }
+
+  @runtimeMethod() public async setDAO(
+    updatedMultiSig: PublicKey
+  ): Promise<void> {
+    assert((await this.CircuitsDAO.get()).isSome.not());
+    assert(updatedMultiSig.isEmpty().not());
+
+    this.CircuitsDAO.set(updatedMultiSig);
   }
 
   /// UPDATE METHODS ==============================================
@@ -66,16 +109,9 @@ export class msUSD extends RuntimeModule<StableConfig> {
     this.collateralRatio.set(ratio);
   }
 
-  @runtimeMethod() public async setDAO(
-    updatedMultiSig: PublicKey
+  @runtimeMethod() public async updateCollateralPrice(
+    price: UInt224
   ): Promise<void> {
-    assert((await this.CircuitsDAO.get()).isSome.not());
-    assert(updatedMultiSig.isEmpty().not());
-
-    this.CircuitsDAO.set(updatedMultiSig);
-  }
-
-  @runtimeMethod() public async setPrice(price: UInt224): Promise<void> {
     assert(
       this.transaction.sender.value.equals((await this.CircuitsDAO.get()).value)
     );
@@ -85,7 +121,15 @@ export class msUSD extends RuntimeModule<StableConfig> {
 
   /// GETTER METHODS ==============================================
 
-  @runtimeMethod() public async getBalance(address: PublicKey): Promise<Field> {
+  @runtimeMethod() public async getCollateralBalance(
+    address: PublicKey
+  ): Promise<Field> {
+    return (await this.collateralBalances.get(address)).value.value || 0;
+  }
+
+  @runtimeMethod() public async getStableBalance(
+    address: PublicKey
+  ): Promise<Field> {
     return (await this.stableBalances.get(address)).value.value || 0;
   }
 
@@ -105,61 +149,92 @@ export class msUSD extends RuntimeModule<StableConfig> {
     return (await this.CircuitsDAO.get()).value.value;
   }
 
-  @runtimeMethod() public async getCollateralPrice(): Promise<UInt224> {
-    return UInt224.Safe.fromField(
-      (await this.collateralPrice.get()).value.value
-    );
-  }
-
   @runtimeMethod() public async getCollateralDeposited(
     address: PublicKey
   ): Promise<Field> {
     return (await this.collateralBalances.get(address)).value.value || 0;
   }
 
+  /// CORE GETTERS ==================================
+
+  @runtimeMethod() public async getCollateralPriceUsd(): Promise<UInt224> {
+    return UInt224.Safe.fromField(
+      (await this.collateralPrice.get()).value.value
+    );
+  }
+
+  @runtimeMethod() public async getTotalCollateralValueUsd(): Promise<UInt224> {
+    const minaUsdRate = await this.getCollateralPriceUsd();
+    const totalCollateral = UInt224.Safe.fromField(
+      (await this.collateralSupply.get()).value.value
+    );
+
+    return totalCollateral.mul(minaUsdRate).div(1e18);
+  }
+
+  @runtimeMethod() public async getStablePrice(): Promise<UInt224> {
+    const totalCollateralValueUsd = await this.getTotalCollateralValueUsd();
+    if (
+      (await this.stableSupply.get()).value.value
+        .equals(Field.from(0))
+        .toBoolean()
+    ) {
+      return UInt224.from(1e18); // Return 1, scaled to 1e18
+    }
+    const scaleFactor = UInt224.from(1e18);
+    return totalCollateralValueUsd
+      .mul(scaleFactor)
+      .div(UInt224.Safe.fromField((await this.stableSupply.get()).value.value));
+  }
+
+  @runtimeMethod() public async getReleasableCollateral(
+    _stablecoinAmount: UInt224,
+    _fee: UInt224
+  ): Promise<UInt224> {
+    return UInt224.from(0);
+  }
+
   /// CORE FUNCTIONS ==============================================
 
   /// LOCKS ===============================================
+  /// CRITERIA FOR LOCKS AND UNLOCKS :
+  /// LOCKS - THE PRICE DEPEGS ABOVE $1.05 OR BELOW $0.95
+  /// UNLOCKS - THE PRICE COMES BACK TO BELOW $1.01 OR ABOVE $0.99
 
   @runtimeMethod() private async unlockSystem(): Promise<void> {
-    assert(
-      (await this.systemLocked.get()).value
-        .and(
-          (await this.collateralPrice.get()).value.value.lessThan(
-            Field.from(10500000000)
-          )
-        )
-        .and(
-          (await this.collateralPrice.get()).value.value.greaterThan(
-            Field.from(9500000000)
-          )
-        )
-    );
+    if ((await this.systemLocked.get()).value.toBoolean()) {
+      this.systemLocked.set(Bool(false));
+    }
     this.systemLocked.set(Bool(false));
   }
 
   @runtimeMethod() private async lockSystem(): Promise<void> {
-    if ((await this.systemLocked.get()).value.not()) {
+    if ((await this.systemLocked.get()).value.not().toBoolean()) {
       this.systemLocked.set(Bool(true));
     }
   }
 
-  @runtimeMethod() private async checkPrice(): Promise<void> {
-    const marketPrice = await this.getCollateralPrice();
+  @runtimeMethod() private async stablePriceChecks(): Promise<Bool> {
+    const marketPrice = await this.getStablePrice();
 
     if (
       marketPrice
-        .lessThan(UInt224.from(9000000000))
-        .or(marketPrice.greaterThan(UInt224.from(11000000000)))
+        .lessThanOrEqual(UInt224.from(95e17))
+        .or(marketPrice.greaterThanOrEqual(UInt224.from(105e18)))
+        .toBoolean()
     ) {
-      this.lockSystem();
+      await this.lockSystem();
+      return Bool(false);
     } else if (
       (await this.systemLocked.get()).value
-        .and(marketPrice.greaterThan(UInt224.from(9500000000)))
-        .and(marketPrice.lessThan(UInt224.from(10500000000)))
+        .and(marketPrice.greaterThanOrEqual(UInt224.from(99e17)))
+        .and(marketPrice.lessThanOrEqual(UInt224.from(101e18)))
+        .toBoolean()
     ) {
-      this.unlockSystem();
+      await this.unlockSystem();
     }
+
+    return Bool(true);
   }
 
   /// MINT / BURNS ============================================
@@ -167,25 +242,28 @@ export class msUSD extends RuntimeModule<StableConfig> {
   @runtimeMethod() public async mint(
     stablecoinAmount: UInt224,
     minaCollateral: UInt224
-  ): Promise<boolean> {
-    this.checkPrice();
-    if (this.systemLocked) {
-      return false;
-    }
+  ): Promise<Bool> {
+    await this.stablePriceChecks();
+    assert((await this.systemLocked.get()).value.not());
 
     const sender = this.transaction.sender.value;
-    // const minaUsdRate = getMinaUsdRate();
-    const minaUsdRate = UInt224.from(0);
+    const minaUsdRate = await this.getCollateralPriceUsd();
     const requiredCollateral = stablecoinAmount
       .mul(
         UInt224.Safe.fromField((await this.collateralRatio.get()).value.value)
       )
       .div(minaUsdRate);
-    if (minaCollateral >= requiredCollateral) {
+    if (minaCollateral.greaterThanOrEqual(requiredCollateral).toBoolean()) {
       const fee = stablecoinAmount
         .mul(UInt224.Safe.fromField((await this.fee.get()).value.value))
-        .div(10000000000);
+        .div(1e18);
+
       const mintedAmount = stablecoinAmount.sub(fee);
+
+      const currentFeeCollected = UInt224.Safe.fromField(
+        (await this.feeCollected.get()).value.value
+      );
+      await this.feeCollected.set(currentFeeCollected.add(fee));
 
       const currentStableBalance =
         UInt224.Safe.fromField(
@@ -197,15 +275,15 @@ export class msUSD extends RuntimeModule<StableConfig> {
         ) || UInt224.from(0);
 
       this.stableBalances.set(sender, currentStableBalance.add(mintedAmount));
+      this.collateralBalances.set(
+        sender,
+        currentCollateralBalance.add(requiredCollateral)
+      );
+
       this.stableSupply.set(
         UInt224.Safe.fromField((await this.stableSupply.get()).value.value).add(
           mintedAmount
         )
-      );
-
-      this.collateralBalances.set(
-        sender,
-        currentCollateralBalance.add(requiredCollateral)
       );
       this.collateralSupply.set(
         UInt224.Safe.fromField(
@@ -213,13 +291,49 @@ export class msUSD extends RuntimeModule<StableConfig> {
         ).add(requiredCollateral)
       );
 
-      return true;
+      return Bool(true);
     }
-    return false;
+    return Bool(false);
   }
 
-  @runtimeMethod() public async burn(amount: UInt224): Promise<Bool> {
-    return Bool(true);
+  @runtimeMethod() public async burn(stablecoinAmount: UInt224): Promise<Bool> {
+    await this.stablePriceChecks();
+    if ((await this.systemLocked.get()).value) {
+      return Bool(false);
+    }
+
+    const sender = this.transaction.sender.value;
+    const currentBalance =
+      UInt224.Safe.fromField(
+        (await this.stableBalances.get(sender)).value.value
+      ) || UInt224.from(0);
+
+    if (currentBalance >= stablecoinAmount) {
+      const fee = stablecoinAmount
+        .mul(UInt224.Safe.fromField((await this.fee.get()).value.value))
+        .div(1e18);
+
+      /// Subtract the stablecoin balance.
+      this.stableBalances.set(sender, currentBalance.sub(stablecoinAmount));
+      const currentSupply = UInt224.Safe.fromField(
+        (await this.stableSupply.get()).value.value
+      );
+      /// Remove the sent-fee from circulation
+      this.stableSupply.set(currentSupply.sub(stablecoinAmount.sub(fee)));
+
+      const currentFeeCollected = UInt224.Safe.fromField(
+        (await this.feeCollected.get()).value.value
+      );
+      await this.feeCollected.set(currentFeeCollected.add(fee));
+
+      const toReleaseCollateral = await this.getReleasableCollateral(
+        stablecoinAmount,
+        fee
+      );
+      // this.distributeFees(fee);
+      return Bool(true);
+    }
+    return Bool(false);
   }
 
   @runtimeMethod() public async transfer(
